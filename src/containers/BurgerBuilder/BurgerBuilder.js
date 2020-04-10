d3f6f346fb1d5a8a3d709c9a0b8c151da329f115
unknown <nhussaini@luc.edu>
2020-04-09 21:03:29 -0700

    displayed the price of burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,7 +31,7 @@ class BurgerBuilder extends Component {
         };
         updatedIngredients[type] = updatedCount;
         const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.oldPrice;
+        const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
 
@@ -48,7 +48,7 @@ class BurgerBuilder extends Component {
         };
         updatedIngredients[type]=updatedCount;
         const priceDeduction = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.oldPrice;
+        const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice-priceDeduction;
         this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
 
@@ -70,6 +70,7 @@ class BurgerBuilder extends Component {
                  ingredientAdded={this.addIngredientHandler}
                  ingredientRemoved={this.removeIngredientHandler}
                  disabled={disabledInfo}
+                 price={this.state.totalPrice}
                  />
 
             </Aux>
