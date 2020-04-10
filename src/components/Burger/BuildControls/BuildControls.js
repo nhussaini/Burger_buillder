5f55d788905dc6a4ef962a609217f82bdcb8cfe2
unknown <nhussaini@luc.edu>
2020-04-10 09:45:57 -0700

    added the order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,7 @@ const buildControls = (props) => (
              removed={() =>props.ingredientRemoved(cntrl.type)}
              disabled={props.disabled[cntrl.type]}/>
         ))}
+        <button className={classes.OrderButton} disabled={!props.purchasable}>ORDER NOW</button>
 
     </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,19 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+        .map(igKey=>{
+            return ingredients[igKey];
+        })
+        .reduce((sum,el) =>{
+            return sum+el;
+        },0);
+        this.setState({purchasable: sum > 0}); 
     }
 
     addIngredientHandler=(type) =>{
@@ -34,6 +46,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
 
 
     }
@@ -51,6 +64,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice-priceDeduction;
         this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
 
 
     }
@@ -70,6 +84,7 @@ class BurgerBuilder extends Component {
                  ingredientAdded={this.addIngredientHandler}
                  ingredientRemoved={this.removeIngredientHandler}
                  disabled={disabledInfo}
+                 purchasable={this.state.purchasable}
                  price={this.state.totalPrice}
                  />
 
