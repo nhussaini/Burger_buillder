530f452de934b6f8adaa9d2a893a46043d425ecf
unknown <nhussaini@luc.edu>
2020-04-13 11:05:19 -0700

    worked on responsive adjustment

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,10 @@ import classes from './Toolbar.css';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
+        <div className={classes.Logo}>
         <Logo />
+        </div>
+        
         <nav>
             <NavigationItems />
         </nav>
