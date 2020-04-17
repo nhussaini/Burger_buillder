bea68c271039c6874740d1989bac8e8b068c722c
unknown <nhussaini@luc.edu>
2020-04-17 08:07:24 -0700

    sent a post request to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -79,7 +80,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler =() =>{
-        alert('You continued!');
+        //alert('You continued!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer :{
+                name: 'Nasrullah',
+                address: {
+                    street: '50 Billington Cres',
+                    zipCode: '234',
+                    country: 'Canada'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
     }
     render() {
         const disabledInfo ={
