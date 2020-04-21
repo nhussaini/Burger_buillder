c641e9ace9cf8f468497b0b225e55ceb95f62822
unknown <nhussaini@luc.edu>
2020-04-21 10:25:07 -0700

    navigated to the Contact Data Comp

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = { 
@@ -23,7 +25,7 @@ class Checkout extends Component {
         this.props.history.goBack(); 
     }
     checkoutContinuedHandler =() =>{
-        this.props.history.replace('/chekcout/contact-data');
+        this.props.history.replace('/checkout/contact-data');
     }
     render(){
         return (
@@ -32,6 +34,7 @@ class Checkout extends Component {
                  ingredients={this.state.ingredients}
                  checkoutCancelled={this.checkoutCancelledHandler}
                  checkoutContinued={this.checkoutContinuedHandler} />
+                 <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         );
     }
