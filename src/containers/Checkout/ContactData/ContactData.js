5c65869a8134a40c65e97a101c6a2c7d2b3cab94
unknown <nhussaini@luc.edu>
2020-04-25 07:31:03 -0700

    handled overall form validity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,10 +84,12 @@ class ContactData extends Component{
                         {value:'cheapest', displayValue: 'cheapest'}
                     ]
                 },
-                value:''
+                value:'',
+                valid:true
             }
 
        },
+        formIsValid:false,
         loading: false
 
     }
@@ -141,8 +143,14 @@ class ContactData extends Component{
         updatedFormElement.valid=this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched= true;
         updatedOrderForm[inputIdentifier]= updatedFormElement;
-        console.log(updatedFormElement);
-        this.setState({orderForm: updatedOrderForm});
+        
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm){
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
+
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
 
     }
     render(){
@@ -166,7 +174,7 @@ class ContactData extends Component{
                             touched={formElement.config.touched}
                             changed={(event)=> this.inputChangedhandler(event, formElement.id)} />
                     ))}
-                    <Button btnType="Success">ORDER</Button>
+                    <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
                 </form>
         );
         if(this.state.loading){
