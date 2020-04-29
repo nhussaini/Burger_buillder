b77dd74b1aa75ee4af0276bce15a8613f65803dd
unknown <nhussaini@luc.edu>
2020-04-29 10:49:57 -0700

    adjusted checkout and contact data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -105,17 +105,18 @@ class BurgerBuilder extends Component {
         // .catch(error => {
         //     this.setState({loading:false,purchasing:false});            
         // });
-        const queryParams = [];
-        for(let i in this.state.ingredients){
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
+        // const queryParams = [];
+        // for(let i in this.state.ingredients){
+        //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        // }
+        // queryParams.push('price=' + this.state.totalPrice);
+        // const queryString = queryParams.join('&');
 
-        this.props.history.push({
-            pathname:'/checkout',
-            search: '?' + queryString
-        });
+        // this.props.history.push({
+        //     pathname:'/checkout',
+        //     search: '?' + queryString
+        // });
+        this.props.history.push('/checkout');
     }
     render() {
         const disabledInfo ={
