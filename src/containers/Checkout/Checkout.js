7d2e437dafca074e9ff2abcbcb7d6c5d0bf47672
unknown <nhussaini@luc.edu>
2020-05-02 16:44:48 -0700

    handled purchases and updated the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,8 +3,9 @@ import { Route,Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-
+import * as actions from '../../store/actions/index';
 class Checkout extends Component {
+  
     // state = { 
     //     ingredients: null,
     //     totalPrice: 0
@@ -33,8 +34,10 @@ class Checkout extends Component {
     render(){
         let summary = <Redirect to="/" />
         if (this.props.ings){
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/"/>: null;
             summary =(
                 <div>
+                    {purchasedRedirect}
                       <CheckoutSummary
                              ingredients={this.props.ings}
                              checkoutCancelled={this.checkoutCancelledHandler}
@@ -53,8 +56,11 @@ class Checkout extends Component {
 
 const mapStateToProps = state =>{
     return {
-        ings:state.burgerBuilder.ingredients
+        ings:state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     };
-} 
+};
+
+
 
 export default connect (mapStateToProps) (Checkout);
