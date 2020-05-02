7d619c18e8b45e7a8151d81cb69e82bd315afbe4
unknown <nhussaini@luc.edu>
2020-05-02 07:15:23 -0700

    fetched ingredients asycly

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,15 +8,14 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import axios from '../../axios-orders';
 import * as burgerBuilderActions from '../../store/actions/index';
+import axios from '../../axios-orders';
 
 
 class BurgerBuilder extends Component {
     state = {
-        purchasing: false,
-        loading: false,
-        error: false
+        purchasing: false
+        
     }
 
     componentDidMount () {
@@ -150,10 +149,6 @@ class BurgerBuilder extends Component {
                 price={this.props.price}
                 purchaseCancelled={this.purchaseCancelHandler}
                 purchaseContined={this.purchaseContinueHandler}/>;
-        }
-        if(this.state.loading){
-            orderSummary = <Spinner />;
-             
         }
         
         return (
