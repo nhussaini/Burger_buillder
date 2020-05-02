8ecfcf79526617ee27a95062070ac21afb0e71ff
unknown <nhussaini@luc.edu>
2020-05-02 14:41:54 -0700

    redirected to improve ux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Route } from 'react-router-dom';
+import { Route,Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -31,18 +31,24 @@ class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data');
     }
     render(){
-        return (
-            <div>
-                <CheckoutSummary
-                 ingredients={this.props.ings}
-                 checkoutCancelled={this.checkoutCancelledHandler}
-                 checkoutContinued={this.checkoutContinuedHandler} />
+        let summary =<Redirect to="/" />
+        if (this.props.ings){
+            summary =(
+                <div>
+                      <CheckoutSummary
+                             ingredients={this.props.ings}
+                             checkoutCancelled={this.checkoutCancelledHandler}
+                             checkoutContinued={this.checkoutContinuedHandler} />
                  <Route path={this.props.match.path + '/contact-data'}
-                //   render={(props)=>(<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)}
-                        component={ContactData}
-                />
-            </div>
-        );
+                 //         render={(props)=>(<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)}
+                             component={ContactData}
+                 />
+
+                </div>
+              
+            );
+        }
+        return summary;
     }
 }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -94,7 +94,6 @@ class ContactData extends Component{
 
        },
         formIsValid:false,
-        loading: false
 
     }
     orderHandler =(event) => {
@@ -174,7 +173,7 @@ class ContactData extends Component{
                     <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
                 </form>
         );
-        if(this.state.loading){
+        if(this.props.loading){
             form=<Spinner />;
         }
         return(
@@ -191,12 +190,16 @@ class ContactData extends Component{
 const mapStateToProps = state =>{
     return{
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     };
 } 
 
 const mapDispatchToProps =dispatch =>{
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    }
+    
 }
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData,axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData,axios));
