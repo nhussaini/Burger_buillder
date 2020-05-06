993f17552756a2ea384fc672e261a7ea87e4317c
unknown <nhussaini@luc.edu>
2020-05-06 08:33:41 -0700

    refactored reducers

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,15 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error:null,
+    loading: false
+};
+
+const reducer = (state, action) =>{
+   
+
+};
+
+export default reducer;
