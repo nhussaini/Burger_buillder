9993d00c0b81116e6226845ef6d0b3c0b0cc0fc2
unknown <nhussaini@luc.edu>
2020-05-08 09:19:37 -0700

    forwarded unauthenticated user to sign up page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,7 +75,12 @@ class BurgerBuilder extends Component {
     // }
 
     purchaseHandler =()=> {
-        this.setState({purchasing: true});
+        if(this.props.isAuthenticated){
+            this.setState({purchasing: true});
+        }else{
+             this.props.history.push('/auth');
+        }
+        
     }
 
     purchaseCancelHandler =() =>{
@@ -143,6 +148,7 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         price={this.props.price}
                    />
                  </Aux>);
@@ -170,7 +176,8 @@ const mapStateToProps = state =>{
     return {
         ings : state.burgerBuilder.ingredients,
         price : state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !==null
     };
 }
 const mapDispatchToProps = dispatch =>{
