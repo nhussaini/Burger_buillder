3238092391aa67e8a96eb560470c6d5c6bf05a39
unknown <nhussaini@luc.edu>
2020-05-11 16:49:54 -0700

    displayed user specific orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,6 +1,7 @@
 import * as actionTypes from  './actionTypes';
 import axios from '../../axios-orders';
 
+
 export const purchaseBurgerSuccess =(id, orderData) =>{
     return {
         type: actionTypes.PURCHASE_BURGER_SUCCESS,
@@ -63,10 +64,11 @@ export const fetchOrdersStart = () =>{
     };
 };
 
-export const fetchOrders = (token) =>{
+export const fetchOrders = (token,userId) =>{
     return dispatch =>{
         dispatch(fetchOrdersStart());
-        axios.get('/orders.json?auth='+token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId +'"';  
+        axios.get('/orders.json' + queryParams)
         .then(res =>{
             const fetchedOrders= [];
             for(let key in res.data){
